2b909e4d3e884eb1471588e3231cf3b22e1ff10b
Rashid Abidokov <N23R35@gmail.com>
2020-09-08 22:56:01 +0300

    ridge regression done

--- 4 Неделя/linear_regression_ridge.py
+++ 4 Неделя/linear_regression_ridge.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
+import scipy as sp
 from sklearn.feature_extraction import text
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import Ridge
 
-data_train_link = 'salary-train.csv'
-data_test_link = 'salary-test-mini.csv'
+data_train_link = 'C:\\Учеба\\Машобчик\\4 Неделя\\salary-train.csv'
+data_test_link = 'C:\\Учеба\\Машобчик\\4 Неделя\\salary-test-mini.csv'
 data_train = pd.read_csv(data_train_link, delimiter=',')
 data_test = pd.read_csv(data_test_link, delimiter=',')
 
@@ -16,16 +18,30 @@ data_test['FullDescription'] = descr_test_lowcase.replace('[^a-zA-Z0-9]', ' ', r
 
 # tfid
 tfid_vectorizer = text.TfidfVectorizer(min_df=5)
-X_train = tfid_vectorizer.fit_transform(data_train['FullDescription'])
-X_test = tfid_vectorizer.transform(data_test['FullDescription'])
+data_train_text = tfid_vectorizer.fit_transform(data_train['FullDescription'])
+data_test_text = tfid_vectorizer.transform(data_test['FullDescription'])
 
 # В LocationNormalized и ContractTime заменяем пропуски на nan
-# и в кучу булевых переменных
 data_train['LocationNormalized'].fillna('nan', inplace=True)
 data_train['ContractTime'].fillna('nan', inplace=True)
 data_test['LocationNormalized'].fillna('nan', inplace=True)
 data_test['ContractTime'].fillna('nan', inplace=True)
 
+# Превращаем категориальные признаки string в наборы bool
 dict_vectorizer = DictVectorizer()
+data_train_categ = dict_vectorizer.fit_transform(data_train[['LocationNormalized', 'ContractTime']].to_dict('records'))
+data_test_categ = dict_vectorizer.transform(data_test[['LocationNormalized', 'ContractTime']].to_dict('records'))
 
-#print(data_test)
+# Объединяем данные текста и категориальных признаков
+data_train_sum = sp.sparse.hstack([data_train_text, data_train_categ])
+data_test_sum = sp.sparse.hstack([data_test_text, data_test_categ])
+print(data_train_sum.shape)
+print(data_test_sum.shape)
+
+# Обучаем регрессию
+regr = Ridge(alpha=1, random_state=241)
+regr.fit(data_train_sum, data_train['SalaryNormalized'])
+
+# Прогнозы для теста
+test_pred = regr.predict(data_test_sum)
+print(np.round(test_pred, 2))
